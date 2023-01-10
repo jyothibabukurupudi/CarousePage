76f59e9362290fe2f204636a4ec26497bd968f8b
LENOVO <LENOVO@DESKTOP-DE76UHQ>
2023-01-10 15:53:01 +0530

    Add project files.

---
+++ App6/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App6
+{
+
+    public class model
+    {
+        public string Name { get; set; }
+    }
+    public partial class MainPage : CarouselPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            ItemsSource= new List<model> { 
+                new model{Name="hello"},
+                new model {Name="hi"},
+                new model{Name="hello"},
+                new model{Name="hello"},
+                new model{Name="hello"},
+            };
+                
+        }
+    }
+}
